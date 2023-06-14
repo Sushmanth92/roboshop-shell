8fc8f4ddf575e1c91b7760b27592b968b509b538
sushmanth <reddi.suhsmanth@gmail.com>
2023-06-14 12:13:16 +0100

    mysql Script

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,29 @@
-yum install nginx -y
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+COMPONENT=frontend
+CONTENT="*"
+source common.sh
 
-systemctl enable nginx
-systemctl restart nginx
+PRINT "Install Nginx"
+yum install nginx -y &>>$LOG
+STAT $?
+
+APP_LOC=/usr/share/nginx/html
+
+DOWNLOAD_APP_CODE
+
+mv frontend-main/static/* .
+
+PRINT "Copy RoboShop Configuration File"
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
+
+PRINT "Update RoboShop Configuration"
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.devopsb69.online/'  -e '/user/ s/localhost/dev-user.devopsb69.online/' -e '/cart/ s/localhost/dev-cart.devopsb69.online/' -e '/shipping/ s/localhost/dev-shipping.devopsb69.online/' -e '/payment/ s/localhost/dev-payment.devopsb69.online/' /etc/nginx/default.d/roboshop.conf
+STAT $?
+
+PRINT "Enable Nginx Service"
+systemctl enable nginx &>>$LOG
+STAT $?
+
+PRINT "Start Nginx Service"
+systemctl restart nginx &>>$LOG
+STAT $?
